2904553728a5e2dd127d4c4c937b14cbf66d1581
Acesif <asifpalak@gmail.com>
2023-02-18 01:57:41 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css'
-import Nav from './nav'
+import './App.css';
+import Nav from './nav';
 // import image from './NewHorizons.png'
-import Btn from './Btn'
-import DarkMode from './darkMode'
+import Btn from './Btn';
+import DarkMode from './darkMode';
+import Child from './Child';
+import React from 'react'
 
 // function Image(prop){
 //   const imgTag = <img src={image} alt='new horizon'></img>
@@ -11,15 +13,23 @@ import DarkMode from './darkMode'
 function Greeting({name}){
   return <h1>Hello, {name}</h1>
 }
-
+const date = new Date();
+let bool = true;
 function App(prop) {
+  const [greet, setGreet] = React.useState('Hola');
+  function clickHandler(){
+    bool? setGreet("sup") : setGreet("hola")
+    bool = !bool;
+  }
   return  <>
             <Nav info="chink"/>
             {/* <Image /> */}
             <Btn val={true}/>
             <DarkMode />
             <Greeting name={"nigger"}/>
-            <h1>{prop.title}</h1>
+            <h1>{prop.title} {greet}</h1>
+            <button onClick={clickHandler}>Hehe</button>
+            <Child msg={date.toLocaleTimeString()}/>
           </>
 }
 export default App;
---
+++ src/Child.js
@@ -0,0 +1,6 @@
+function Child(prop){
+    return (
+        <h1>{prop.msg}</h1>
+    )
+}
+export default Child;
